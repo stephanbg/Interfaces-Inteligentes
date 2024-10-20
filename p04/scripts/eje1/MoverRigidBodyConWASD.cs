1ac59fe7b27d753ba41a8c2ad0fa5c5e4ed6bcf9
Stephan Brommer Gutiérrez <alu0101493497@ull.edu.es>
2024-10-20 03:16:07 +0100

    Create MoverRigidBodyConWASD.cs

---
+++ p04/scripts/eje1/MoverRigidBodyConWASD.cs
@@ -0,0 +1,38 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Interfaces Inteligentes
+ *
+ * @author Stephan Brommer Gutiérrez
+ * @since 16/10/2024
+ * @file MoverRigidBodyConWASD.cs
+ * @brief Este script permite mover un RigidBody perfecto, sin usar transform, solo con
+ *        métodos como MovePosition. Eje1
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clase que permite mover un objeto Rigidbody utilizando las teclas de WASD.
+public class MoverRigidBodyConWASD : MonoBehaviour {
+  // Velocidad a la que se moverá el objeto.
+  public float velocidad = 5f;
+  // Referencia al componente Rigidbody del objeto.
+  private Rigidbody rb;
+  // Inicializa la referencia al Rigidbody en el inicio.
+  private void Start() {
+    rb = GetComponent<Rigidbody>();
+  }
+  // Actualiza la posición del objeto según la entrada del teclado en cada frame.
+  private void FixedUpdate() {
+    float horizontal = 0f, vertical = 0f;
+    if (Input.GetKey(KeyCode.W)) vertical = 1f;
+    if (Input.GetKey(KeyCode.S)) vertical = -1f;
+    if (Input.GetKey(KeyCode.A)) horizontal = -1f;
+    if (Input.GetKey(KeyCode.D)) horizontal = 1f;
+    Vector3 movimiento = new Vector3(horizontal, 0, vertical) * velocidad * Time.deltaTime;
+    rb.MovePosition(rb.position + movimiento);
+  }
+}
