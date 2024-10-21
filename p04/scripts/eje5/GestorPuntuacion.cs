2df281338e61708a7c4aa7e7ea5c495203dee28c
Stephan Brommer Gutiérrez <alu0101493497@ull.edu.es>
2024-10-21 11:01:40 +0100

    Update GestorPuntuacion.cs

--- p04/scripts/eje5/GestorPuntuacion.cs
+++ p04/scripts/eje5/GestorPuntuacion.cs
@@ -1,14 +1,33 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Interfaces Inteligentes
+ *
+ * @author Stephan Brommer Gutiérrez
+ * @since 17/10/2024
+ * @file GestorPuntuacion.cs
+ * @brief Este script gestiona la puntuación del juego, permitiendo sumar 
+ *        puntos y mostrar la puntuación actual. Eje5
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Clase que gestiona la puntuación del juego.
 public class GestorPuntuacion : MonoBehaviour {
+  private static int puntuacion = 0; // Almacena la puntuación actual del juego.
+  // Suma una cantidad de puntos a la puntuación actual.
   public static void sumarPuntos(int puntos) {
     puntuacion += puntos;
   }
+  // Muestra la puntuación actual en la consola.
   public static void mostrarPuntos() {
     Debug.Log("Puntuación actual: " + puntuacion);
   }
-  public static int getPuntuacion() { return puntuacion; }
-  private static int puntuacion = 0;
+  // Devuelve la puntuación actual.
+  public static int getPuntuacion() { 
+    return puntuacion; 
+  }
 }
