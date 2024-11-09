7df3395c4689d28c391b0304f3b7bda8535ee1b3
Stephan Brommer Gutiérrez <alu0101493497@ull.edu.es>
2024-11-09 10:04:01 +0000

    Create NotificadorOnClick.cs

---
+++ escenas-CardBoard2/scripts/Eje8/NotificadorOnClick.cs
@@ -0,0 +1,23 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Interfaces Inteligentes
+ *
+ * @author Stephan Brommer Gutiérrez
+ * @since 7/11/2024
+ * @file NotificadorOnClick.cs
+ * @brief Clase que notifica eventos de clic sobre un objeto en Unity. Eje8
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clase que gestiona la notificación de clics en el objeto.
+public class NotificadorOnClick : MonoBehaviour {
+  public delegate void click(); // Delegado para el evento OnClick.
+  public event click OnClick; // Evento que se activa al hacer clic en el objeto.
+  // Método que se llama cuando se detecta un clic del ratón sobre el objeto.
+  private void OnMouseDown() { OnClick(); }
+}
